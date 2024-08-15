fd11211cc8dc641d5a7868c63959f1684174c885
edenbar3 <edenbar3@gmail.com>
2024-08-16 01:02:34 +0300

    update sign in with google

---
+++ laflame/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Fragment } from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import { ReactComponent as CactusLogo } from '../../assests/cactus.svg'
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'>
+                    <CactusLogo className='logo' />
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>
+                        Shop
+                    </Link>
+                    <Link className='sign-in' to='/sign-in'>
+                        SignIn
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation;
---
+++ laflame/src/utils/firebase/firbase.utils.js
@@ -0,0 +1,53 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwcbU6zhsnbAbpMFf7_YggtOQvmXIj-BU",
+  authDomain: "laflame-db.firebaseapp.com",
+  projectId: "laflame-db",
+  storageBucket: "laflame-db.appspot.com",
+  messagingSenderId: "1089201806149",
+  appId: "1:1089201806149:web:c7c37b4a0b28ca6eddbc2c"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
